de42258bfe63b3b20c9fa9ba8e2b95c178b43b7a
Mostafa Sholkamy <shalkamm@gmail.com>
2019-05-27 12:18:06 +0200

    added graphql as peer dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const card = require('./card')
 const binding = require('./binding')
 const fixedString = require('./fixedString')
+const schema = require('./schema')
 
 const directives = { card, binding, fixedString }
-module.exports = directives
+module.exports = { directives, schema }
